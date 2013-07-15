6153e9da0fcd92d15761d09e9710f38fc660f3df
Gildus <gildus@gmailcom>
2013-07-15 10:07:30 -0500

    Archivos iniciales

--- js/visitas.js
+++
@@ -1,9 +0,0 @@
-function validarFormulario(form)
-{
-	if(form.mensaje.value.length > 0) {
-		return true;
-	} else {
-		alert('Ingresa un mensaje...');
-		return false;
-	}
-}
--- salir.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['nick']);
-unset($_SESSION['IdUsu']);
-
-header('Location: index.php');
